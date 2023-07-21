756a5ddd6fb6737c9e8638574940ea96966d6d18
Sail100 <85443860+Sail100@users.noreply.github.com>
2023-07-21 17:54:23 -0400

    Create cg.lua

---
+++ cg.lua
@@ -0,0 +1,7 @@
+local bedwars = 6872274481
+	
+if bedwars == bedwars then
+	print("Game is whitelisted/n Script can run.")
+else
+	game.Players.LocalPlayer.Kick("AlSploit/n This game is not whitelisted you should be in roblox bedwars to be able to use this script. If you think this is wrong. please report it the GUI Library Devs discord: desiraqble")
+end
