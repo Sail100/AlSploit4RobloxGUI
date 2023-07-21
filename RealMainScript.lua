b2218af0173c8af16f142f2587d1682f4f63f763
Sail100 <85443860+Sail100@users.noreply.github.com>
2023-07-21 18:01:41 -0400

    Update RealMainScript.lua

--- RealMainScript.lua
+++ RealMainScript.lua
@@ -16,7 +16,7 @@ shared.AlSploit = AlSploit
 local queueteleport = queue_on_teleport
 
 local TeleportString = [[
-  loadstring(game:HttpGet("E", true))
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Sail100/AlSploit4RobloxGUI/main/RealMainScript.lua", true))
 ]]
 
 queueteleport(TeleportString)
@@ -24,8 +24,20 @@ queueteleport(TeleportString)
 local queueteleport2 = queue_on_teleport
 
 local TeleportString2 = [[
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Sail100/AlSploit4RobloxGUI/main/ctg.lua", true))
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Sail100/AlSploit4RobloxGUI/main/ctg.lua, true))
 ]]
+
+local FunctionsLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/Libraries/FunctionsHandler.lua", true))()
+shared.funcslib = FunctionsLibrary
+
+
+local bedwars = 6872274481
+	
+if bedwars == bedwars then
+	print("Game is whitelisted/n Script can run.")
+else
+	game.Players.LocalPlayer.Kick("AlSploit/n This game is not whitelisted you should be in roblox bedwars to be able to use this script. If you think this is wrong. please report it the GUI Library Devs discord: desiraqble")
+end
 
 queueteleport2(TeleportString2)
 
@@ -38,10 +50,10 @@ if isfolder("AlSploit/assets") == false then
 	makefolder("AlSploit/assets")
 end
 
-local AnyGame = [[
-loadstring(game:HttpGet("-- not yet --",
-true))()
-]]
+local AnyGame = [loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua
+", true))()]
+
+
 if isfolder("AlSploit/CustomModules") == false then
 	makefolder("AlSploit/CustomModules")
 end
@@ -69,7 +81,7 @@ function MainLoaded()
   elseif isfile("AlSploit/CustomModules/"..game.PlaceId..".lua") then
     loadstring(readfile("AlSpoit/CustomMod/"..game.PlaceId..".lua"))()
   else
-    loadstring(game:HttpGet("unknown"))()
+    loadstring(game:HttpGet("loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()"))()
   end
 end
 --]]
@@ -78,11 +90,11 @@ task.spawn(function()
   MainLoaded()
 end)
 
---[[if not shared.FuncsConnect then--]]
+if not shared.FuncsConnect then
   task.spawn(function()
   	repeat
   	  task.wait()
-  		--[[shared.FuncsConnect = true--]]
-  	--[[until shared.FuncsConnect--]]
+  		shared.FuncsConnect = true
+  	until shared.FuncsConnect
   end)
 end
