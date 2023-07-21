889a2eb669bcdb54721d5c3c84f469130683366c
Sail100 <85443860+Sail100@users.noreply.github.com>
2023-07-21 17:40:27 -0400

    Create ctg.lua

---
+++ ctg.lua
@@ -0,0 +1,13 @@
+do do local v0=Instance.new("Frame");v0.Parent=game.StarterGui.ScreenGui;v0.BackgroundColor3=Color3.fromRGB(255,255,255);v0.BorderColor3=Color3.fromRGB(0,0,0);v0.BorderSizePixel=0;v0.Size=UDim2.new(0,28,0,0);local function v7()local v8=Instance.new("LocalScript",v0);local v9=game.StarterGui;local v10=Instance.new("Script");v10.Parent=v9;local v12=game:GetService("TextChatService");v12.OnIncomingMessage=function(v14)local v15=Instance.new("TextChatMessageProperties");if v14.TextSource then local v16=game:GetService("Players"):GetPlayerByUserId(v14.TextSource.UserId);if (v16.Name=="McAsheleyFriend89") then v15.PrefixText="<font color='##F00D0D'>[AlSploit GUI Library Developer]</font> "   .. "<font color='##0DF0DB'>[SanWareClientX Owner]</font> "   .. v14.PrefixText ;end end return v15;end;end coroutine.wrap(v7)();end end
+-- ⚠️ WARNING: integrity protected!
+--[[
+ .____                  ________ ___.    _____                           __                
+ |    |    __ _______   \_____  \\_ |___/ ____\_ __  ______ ____ _____ _/  |_  ___________ 
+ |    |   |  |  \__  \   /   |   \| __ \   __\  |  \/  ___// ___\\__  \\   __\/  _ \_  __ \
+ |    |___|  |  // __ \_/    |    \ \_\ \  | |  |  /\___ \\  \___ / __ \|  | (  <_> )  | \/
+ |_______ \____/(____  /\_______  /___  /__| |____//____  >\___  >____  /__|  \____/|__|   
+         \/          \/         \/    \/                \/     \/     \/                   
+          \_Welcome to LuaObfuscator.com   (Alpha 0.9.12) ~  Much Love, Ferib 
+
+]]--
+
